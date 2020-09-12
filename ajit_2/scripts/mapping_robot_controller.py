3b0bd2c96ef05c06426947a2356c27255d1e478d
alwinmreji <01fe17bar005@kletech.ac.in>
2020-09-13 00:48:24 +0530

    backup

--- ajit_2/scripts/mapping_robot_controller.py
+++ ajit_2/scripts/mapping_robot_controller.py
@@ -2,14 +2,11 @@
 import rospy
 from sensor_msgs.msg import JointState
 from std_msgs.msg import Float64MultiArray
-# q_s = 15
-# negative = ['Right_Elbow','Right_Shoulder_RotX','Right_Shoulder_RotY','Right_Shoulder_RotZ']
-#[head,left_lower_arm,left_shoulder_joint,left_shoulder,left_upper_arm,left_hand,neck,right_lower_arm,right_shoulder_joint,right_shoulder,right_upper_arm,right_hand]
+
 data_pass = rospy.Publisher('ajit_2/robot_position_controller/command',Float64MultiArray,queue_size = 5)
 
 def splitter(data):
     val = Float64MultiArray()
-    # val.layout.dim = [12]
     val.data = [i*-1 for i in data.position]
     data_pass.publish(val)
     rospy.loginfo(val.data)
@@ -17,9 +14,8 @@ def splitter(data):
 def recive():
      rospy.init_node("reciver",anonymous=True)
      rospy.Subscriber("/dynamixel_workbench/joint_states",JointState,splitter)
-     # rospy.spin()
+
 if __name__ == '__main__':
     recive()
     while(not rospy.is_shutdown()):
-        # splitter()
         rospy.spin()
--- joy_controller/script/joint_controller.py
+++
@@ -1,8 +0,0 @@
-import DynamixelCommand.srv as cmd
-
-def controller():
-    rospy.init_node('gamepad_control')
-    s =
-
-if __name__ == "__main__":
-    controller()
